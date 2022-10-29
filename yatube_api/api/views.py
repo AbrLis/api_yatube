ef6a4099c2d3482f23f5c3ac7160978f9cba4d8d
Andrey Domo <abrli@yandex.ru>
2022-10-29 13:55:27 +0500

    списки заменены кортежами вью api

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -8,7 +8,7 @@ from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
@@ -24,13 +24,13 @@ class GroupViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet,
 ):
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
